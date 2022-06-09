e0a3230e3b37f24e1d30c38db28c5f2343a189c6
Murilo Alves <101125668+Murilo2412@users.noreply.github.com>
2022-06-09 10:32:49 -0300

    docs: 📑 exercício 2

---
+++ exercicio_2.js
@@ -0,0 +1,7 @@
+//EXERCÍCIO 2 - Crie uma função que recebe uma string nome por parâmetro e retorna um array com os nomes que contém na string.
+
+let nome = "Murilo Alves, Pedro Silva, Edmarques Lima";
+
+let lista = nome.split(" ")
+
+console.log(lista)
