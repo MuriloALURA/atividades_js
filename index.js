87404cd50ab188c8afc561e1262aa2322d2c29cd
Murilo Alves <101125668+Murilo2412@users.noreply.github.com>
2022-06-09 10:25:03 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// [LISTA DE EXERCÍCIOS]
+
+// EXERCÍCIO 1 - Crie uma função que recebe uma string por parâmetro e a retorna invertida. Ex: Passando bola deve retornar alob.
+
+// EXERCÍCIO 2 - Crie uma função que recebe uma string nome por parâmetro e retorna um array com os nomes que contém na string. Ex: passando ‘edmarques lima’ deve retornar [‘edmarques’, ‘lima’].
+
+// EXERCÍCIO 3 - Crie uma função que retorna recebe uma lista de nomes e imprime uma mensagem de boas vindas para cada nome da lista, seguindo o template ‘Bem vindo [nome], tenha um ótimo dia.’. Ex: Passando [‘edmarques’] deve imprimir ‘Bem vindo Edmarques, tenha um ótimo dia!’ Fazer com uma lista de pelo menos 10 nomes.
+
+// EXERCÍCIO 4 - Crie uma função que recebe uma lista de números e se, a lista possuir pelo menos 10 números, retorna a lista de forma ordenada, não usar método sort, e se a lista possuir mais de 20 números imprime ‘Lista muito grande’, e se a lista possuir menos de 10 números imprime ‘Lista muito pequena’.
+
+// EXERCÍCIO 5 - Crie uma função que recebe uma lista de elementos, remove o elemento que está na posição do meio no array, imprime o elemento removido, e retorna a lista com os elementos restantes.
+
+// EXERCÍCIO 6 - Crie uma função que recebe uma lista de números e retorna uma nova lista contendo os números divisíveis por 2.
+
+// EXERCÍCIO 7 - Crie um objeto ‘pessoa’ que possui os seguintes atributos: nome, idade, sexo, cpf, data de nascimento e altura. Em seguida percorra o objeto imprimindo cada elemento e seu valor, da seguinte forma: ‘A chave “[nome]” possui o valor “[valor]”’.
+
+// EXERCÍCIO 8 - Crie um objeto literal pessoa com os atributos, nome e idade e métodos acessores e modificadores para os atributos citados.
+
+// EXERCÍCIO 9 - Construa uma classe funcionário que possui os atributos nome, data de contratação e duas outras classes que estendem de funcionário, pf e pj, classe pf deve possuir o atributo cpf, salário e benefícios, e pj deve possuir cnpj e salário. Implementar getters e setters nas classes e construtores.
