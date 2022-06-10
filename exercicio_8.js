fcc32ba84a4d9241b614c7642fc7929d0c435047
Murilo Alves <101125668+Murilo2412@users.noreply.github.com>
2022-06-10 09:05:45 -0300

    docs: 📑 exercício 8

---
+++ exercicio_8.js
@@ -0,0 +1,14 @@
+// EXERCÍCIO 8 - Crie um objeto literal pessoa com os atributos, nome e idade e métodos acessores e modificadores para os atributos citados.
+
+const pessoa = {
+    nome: "Roberto",
+    idade: "32",
+}
+
+pessoa.nome = "Anderson"
+pessoa.idade = "30"
+
+console.log(pessoa)
+
+
+
